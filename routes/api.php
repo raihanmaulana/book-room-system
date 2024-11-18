fe020f45969c3041f0b420c9bc0d53d608df9c49
Raihan <raihanmaulana09@gmail.com>
2024-11-18 22:06:01 +0700

    fix: update

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\KTPApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\KtpController;
+use App\Http\Controllers\API\KTPApiController;
+use App\Http\Controllers\WEB\KTPWebController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -16,6 +17,9 @@ Route::get('/ktp', [KTPApiController::class, 'index']);
 Route::post('/ktp', [KTPApiController::class, 'store'])->name('ktp.store');
 Route::get('ktps/{id}', [KTPApiController::class, 'show']);
 Route::put('/ktp/{id}', [KTPApiController::class, 'update'])->name('api.ktp.update');
-Route::patch('ktps/{id}', [KTPApiController::class, 'update']);
+// Route::patch('ktps/{id}', [KTPApiController::class, 'update']);
 Route::delete('/ktp/{id}', [KTPApiController::class, 'destroy'])->name('ktp.destroy');
 Route::post('/import/csv', [KTPApiController::class, 'importCsv'])->name('import.csv');
+
+Route::get('export/pdf', [KTPWebController::class, 'exportPdf']);
+Route::get('export/csv', [KTPWebController::class, 'exportCsv']);
